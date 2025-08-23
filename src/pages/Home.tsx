fa3441080a9801a35c5c01189e45faff6a833fbf
Swamibakal <145317751+Swamibakal@users.noreply.github.com>
2025-08-23 16:40:09 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,7 @@ export default function Home() {
       className="relative min-h-screen w-full bg-cover bg-center"
       style={{ backgroundImage: "url('/assets/vr-bg.jpg')" }}
     >
+      {/* Dark overlay */}
       <div className="absolute inset-0 bg-black bg-opacity-60 flex flex-col justify-center items-center text-center text-white p-4">
         <h1 className="text-5xl font-bold mb-4">
           Center For Immersive Learning
@@ -15,4 +16,3 @@ export default function Home() {
     </div>
   );
 }
-
