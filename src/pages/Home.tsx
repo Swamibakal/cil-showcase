e3a1f5d6ab1181ef4d4798b97e955c490c7def93
Swamibakal <145317751+Swamibakal@users.noreply.github.com>
2025-08-23 16:34:53 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div
       className="relative min-h-screen w-full bg-cover bg-center"
-      style={{ backgroundImage: "https://github.com/Swamibakal/cil-showcase/blob/main/public/assets/vr-bg.jpg" }}
+      style={{ backgroundImage: "url('/assets/vr-bg.jpg')" }}
     >
       <div className="absolute inset-0 bg-black bg-opacity-60 flex flex-col justify-center items-center text-center text-white p-4">
         <h1 className="text-5xl font-bold mb-4">
@@ -15,3 +15,4 @@ export default function Home() {
     </div>
   );
 }
+
